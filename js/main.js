ff35a7fa12363557bae80479b5c614d06529caaf
Luke Sedmak <luke@bluestonecreative.com>
2014-05-21 17:45:55 -0400

    Initial Commit
    Working Navigation
    - Make Menu text & Icon change to Close when opened.

---
+++ js/main.js
@@ -0,0 +1,52 @@
+/*************************************************/
+/***************Bluestone Menu********************/
+/*************************************************/
+
+/*Make Sure The Nav Always stays to the bottom*/
+
+function sizeHeader() {
+	var windowHeight = $(window).height();
+	var headerHeight = windowHeight - 69;
+	$('#contain-head').css({height: windowHeight});
+	$('#contain-head header').css({top: headerHeight, height: windowHeight});
+	$('#content.home').css({height: headerHeight});
+}
+
+$(document).ready(function(){
+	sizeHeader();
+});
+
+$(window).resize(function(){
+	sizeHeader();
+});
+
+//Menu Click Function
+$('.menu-button').click(function(e) {
+	e.preventDefault();
+	$('body').toggleClass('open');
+	animateMenuItems();
+});
+
+//Animate Menu Items
+function animateMenuItems() {
+	var headerPosition = $('header').offset();
+	var headerPositionTop = headerPosition.top;
+	var navItems = $('header nav ul li');
+	console.log(headerPosition.top);
+	
+	if ( headerPositionTop < 100 ) {
+		$.each(navItems, function(i, item) {
+		    setTimeout(function() {
+		        $(item).removeClass('in');
+		    }, 50 * i);
+		})
+		
+	} else if ( headerPositionTop > 100 ) {
+		$.each(navItems, function(i, item) {
+		    setTimeout(function() {
+		        $(item).addClass('in');
+		    }, 50 * i);
+
+		})
+	}
+};
