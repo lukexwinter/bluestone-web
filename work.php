e0fd58e312df10dae6ef1203610f1e4554c1f261
Luke Sedmak <luke@bluestonecreative.com>
2014-06-09 17:53:42 -0400

    work page
    work page

---
+++ work.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Bluestone Creative</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 9]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+		<?php include 'template/header.php' ?>
+        <div id="content" class="work clearfix">
+        	<a href="" id="roadid" class="project">
+				<h2>Road ID</h2>
+				<span class="grid-marker"></span>
+				<h3>Brand Identity, Packaging, Advertising, Web Design, Strategy</h3>
+				<span class="overlay"></span>
+			</a>
+        	<a href="/amalfi.php" id="amalfi" class="project">
+				<h2>Amalfi<br />Stonebriar</h2>
+				<span class="grid-marker"></span>
+				<h3>Brand Identity, Print, Advertising, Signage, 3D, Web Design, Strategy</h3>
+				<span class="overlay"></span>
+			</a>
+        	<a href="" id="downtek" class="project">
+				<h2>Downtek</h2>
+				<span class="grid-marker"></span>
+				<h3>Branding, Print, Web Design, Advertising, Packaging, Strategy</h3>
+				<span class="overlay"></span>
+			</a>
+        	<a href="" id="cosign" class="project">
+				<h2>Cosign</h2>
+				<span class="grid-marker"></span>
+				<h3>Photography, Video Production, Editing, Web Design</h3>
+				<span class="overlay"></span>
+			</a>
+			
+        	<a href="" id="roadid" class="project">
+				<h2>Road ID</h2>
+				<span class="grid-marker"></span>
+				<h3>Brand Identity, Packaging, Advertising, Web Design, Strategy</h3>
+				<span class="overlay"></span>
+			</a>
+        	<a href="" id="amalfi" class="project">
+				<h2>Amalfi<br />Stonebriar</h2>
+				<span class="grid-marker"></span>
+				<h3>Brand Identity, Print, Advertising, Signage, 3D, Web Design, Strategy</h3>
+				<span class="overlay"></span>
+			</a>
+        	<a href="" id="downtek" class="project">
+				<h2>Downtek</h2>
+				<span class="grid-marker"></span>
+				<h3>Branding, Print, Web Design, Advertising, Packaging, Strategy</h3>
+				<span class="overlay"></span>
+			</a>
+        	<a href="" id="cosign" class="project">
+				<h2>Cosign</h2>
+				<span class="grid-marker"></span>
+				<h3>Photography, Video Production, Editing, Web Design</h3>
+				<span class="overlay"></span>
+			</a>
+        </div>
+
+		
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+		
+		<script>
+		    var bsProject = $('.project');
+			function projectHeight() {
+				var halfHeight = ($(window).height() / 2);
+				console.log(windowHeight);
+				bsProject.css({height: halfHeight});
+			}
+			function projectLoad () {
+				$.each(bsProject, function(i, item) {
+				    setTimeout(function() {
+				        $(item).addClass('in');
+				    }, 250 * i);
+				})
+			}
+			$(window).load(function(){
+				projectHeight();
+				projectLoad();
+			});
+			$(window).resize(function(){
+				projectHeight();
+			});
+			
+		</script>
+		
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
