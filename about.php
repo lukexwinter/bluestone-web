d17786a9949da655d1cb0f1a1fbcdca0a925c032
andyr@bluestonecreative.com <andybluestone>
2014-08-04 14:27:39 -0400

    About Page
    - Added h1 tag
    - Test about page

---
+++ about.php
@@ -0,0 +1,67 @@
+		<?php include $_SERVER['DOCUMENT_ROOT']."/template/header.php" ?>
+		<link rel="stylesheet" href="css/rs-minimal-white.css">
+        <div id="content" class="about">
+			<h1>Let me tell you a little bit about me.</h1>
+			
+					<!--<div class="work half">
+						<img class="lazy" data-original="<?php echo MAINURL.""; ?>/img/amalfi-web-half-1.jpg" alt="" />
+						<img class="lazy" data-original="<?php echo MAINURL.""; ?>/img/amalfi-web-half-2.jpg" alt="" />
+					</div>-->
+		</div>
+				
+
+		
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+		
+		
+			
+			<script>
+				/*Royal Slider*/
+				jQuery(document).ready(function($) {
+				  $('.full-width-slider').royalSlider({
+				    arrowsNav: false,
+				    loop: false,
+				    imageScaleMode: 'fill',
+				    arrowsNavAutoHide: false,
+				    autoScaleSlider: true, 
+				    controlNavigation: 'bullets',
+				    thumbsFitInViewport: false,
+				    navigateByClick: true,
+				    startSlideId: 0,
+				    autoPlay: false,
+				    transitionType:'move',
+				    globalCaption: false
+				  });
+				});
+				
+				/*Lazy Load*/	
+				$("img.lazy").lazyload({
+				    effect : "fadeIn"
+				});
+				
+				// Trigger the 
+				$(window).scroll(function() {
+					if ( $('#scroll-video').visible( true ) == true ) {
+						$('#scroll-video').get(0).play()
+					} else {
+						$('#scroll-video').get(0).pause();
+					}
+				});
+			</script>
+		
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
